46e3bc4d902dcc5ef175c4adecf6129dd7dff171
Steven Huwig <shuwig@courseload.com>
2014-02-19 13:05:56 -0500

    Commit a version of generated Nuxeo schema to GitHub.

--- src/main/scripts/copy_xsd.py
+++ src/main/scripts/copy_xsd.py
@@ -1,11 +1,23 @@
 import os
 import xml.dom.minidom
 
-SOURCE_DIR = "../../../target/generated-sources/schema"
-TARGET_DIR = "../../../src/main/resources/schema"
+SOURCE_DIR = "../../../target/generated-sources"
+TARGET_DIR = "../../../src/main/resources"
 
-for filename in os.listdir(SOURCE_DIR):
-    data = xml.dom.minidom.parse(os.path.join(SOURCE_DIR, filename))
-    pretty_xml_string = data.toprettyxml().encode('UTF-8')
-    with open(os.path.join(TARGET_DIR, filename), 'w') as output_file:
-        output_file.write(pretty_xml_string)
+
+def pretty_copy(source_dir, target_dir):
+    for filename in os.listdir(source_dir):
+        data = xml.dom.minidom.parse(os.path.join(source_dir, filename))
+        pretty_xml_string = data.toprettyxml().encode('UTF-8')
+        with open(os.path.join(target_dir, filename), 'w') as output_file:
+            output_file.write(pretty_xml_string)
+
+
+if __name__ == '__main__':
+    schema_target = os.path.join(TARGET_DIR, 'schema')
+    osgi_target = os.path.join(TARGET_DIR, 'OSGI-INF')
+    for p in [schema_target, osgi_target]:
+        if not os.path.exists(p):
+            os.makedirs(p)
+    pretty_copy(os.path.join(SOURCE_DIR, 'schema'), schema_target)
+    pretty_copy(os.path.join(SOURCE_DIR, 'osgi'), osgi_target)
